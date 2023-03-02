fb4063df4eeba7855732bb041fcf287440797a34
phuctq32 <phuctq32@gmail.com>
2023-03-02 20:15:56 +0700

    configure cloudinary & setup upload image

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,8 +8,8 @@ const router = Router();
 // POST
 router.post("/signup", signupValidations, validationErrorHandler, authController.signup);
 
-router.post('/forgot-password', forgotPasswordValidations, validationErrorHandler, authController.forgotPassword);
+router.post("/forgot-password", forgotPasswordValidations, validationErrorHandler, authController.forgotPassword);
 
-router.post('/reset-password/:token', resetPasswordValidations, validationErrorHandler, authController.resetPassword);
+router.post("/reset-password/:token", resetPasswordValidations, validationErrorHandler, authController.resetPassword);
 
 export default router;

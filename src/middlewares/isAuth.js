8257a1a9da66e0783c7c3a2088b761d515b1ad5b
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-03-03 19:28:34 +0700

    Update isAuth.js

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -13,7 +13,7 @@ const isAuth = async (req, res, next) => {
   console.log(token);
   try {
     const result = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = result.userId;
+    req.userID = result.userID;
     req.role = result.role;
     next();
   } catch (error) {

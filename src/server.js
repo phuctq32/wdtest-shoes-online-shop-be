123bd6f8491fc5631ec15233885bf1b4c9895a87
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-03-03 22:53:32 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import * as app from "./app.js";
 import db from "./configs/database.js";
-import { addUser } from "./utils/addData.js";
 app.init();
 
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
@@ -8,5 +7,3 @@ db.once("open", () => {
   console.log("DB connected");
   app.run();
 });
-
-addUser("testing@gmail.com", "123213", "shoe", true);
